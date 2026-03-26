7104856d1a3bc790078a1739ac2be00e1c06c5bb
Will <wibiduma@gmail.com>
2026-03-26 09:25:09 +0800

    score->highscore

--- StrokeNet/Server/src/login.cpp
+++ StrokeNet/Server/src/login.cpp
@@ -57,7 +57,7 @@ void UserStore::save()
             rapidjson::Value(user.c_str(), alloc), alloc);
         obj.AddMember("password",
             rapidjson::Value(pass.c_str(), alloc), alloc);
-        obj.AddMember("score", rapidjson::Value(0, alloc), alloc);
+        obj.AddMember("highscore", rapidjson::Value(0), alloc);
         arr.PushBack(obj, alloc);
     }
     doc.AddMember("users", arr, alloc);
