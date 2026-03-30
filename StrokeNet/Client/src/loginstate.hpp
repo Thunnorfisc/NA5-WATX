522acf095e4bcb65d6938e67eded17b0e0b0ce14
Will <wibiduma@gmail.com>
2026-03-30 20:46:53 +0800

    hey hey you you I did the login UI

--- StrokeNet/Client/src/loginstate.hpp
+++ StrokeNet/Client/src/loginstate.hpp
@@ -10,8 +10,8 @@
     Reproduction or diclosure of this file or its contents without the prior
     written consent of DigiPen Institute of Technology is prohibited. */
 
-/* End Header
-***********************************************************************/
+    /* End Header
+    ***********************************************************************/
 #pragma once
 
 #include "state.hpp"
@@ -33,12 +33,13 @@ public:
     void render() override;
 
 private:
-    enum class Field { Username, Password, COUNT };
+    enum class Field { Username, Password, ServerIP, Port, COUNT };
     static constexpr int FIELD_COUNT = static_cast<int>(Field::COUNT);
 
     void updateLayout();
     void selectField(Field field);
     void attemptLogin();
+    void attemptDirectConnect();  // TODO: implement direct connection using IP and port
     void attemptCreateAccount();
 
     sf::Font m_font;
@@ -52,9 +53,15 @@ private:
     std::array<std::optional<sf::Text>, FIELD_COUNT> m_fieldTexts;
     Field m_activeField = Field::Username;
 
-    // buttons
-    sf::RectangleShape m_loginButton;
-    sf::Text m_loginButtonText;
+    // "Connect via:" caption + buttons
+    sf::Text m_connectViaText;
+    sf::RectangleShape m_broadcastButton;
+    sf::Text m_broadcastButtonText;
+    sf::RectangleShape m_directButton;
+    sf::Text m_directButtonText;
+
+    // "New?" caption + create account button
+    sf::Text m_newText;
     sf::RectangleShape m_createButton;
     sf::Text m_createButtonText;
 
